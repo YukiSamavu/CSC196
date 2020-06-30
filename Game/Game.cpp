febefea33129c86a251cd793edc0cdc8d2b92d22
Yukisan12 <61423439+Yukisan12@users.noreply.github.com>
2020-06-30 17:11:53 -0400

    Ending of second class

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -7,8 +7,16 @@
 #include "Math/Random.h"
 #include <iostream>
 
+const size_t NUM_POINTS = 40;
+std::vector<nc::Vector2> points;
+
 bool Update(float dt)
 {
+	for (size_t i = 0; i < NUM_POINTS; i++)
+	{
+		points[i] = nc::Vector2{ nc::random(0.0f, 800.0f), nc::random(0.0f, 600.0f) };
+	}
+
 	return false;
 }
 
@@ -16,10 +24,22 @@ void Draw(Core::Graphics& graphics)
 {
 	graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
 	graphics.DrawLine(nc::random(0.0f, 800.0f), nc::random(0.0f, 600.0f), nc::random(0.0f, 800.0f), nc::random(0.0f, 600.0f));
+
+	for (size_t i = 0; i < NUM_POINTS - 1; i++)
+	{
+		nc::Vector2 p1 = points[i];
+		nc::Vector2 p2 = points[i + 1];
+
+		graphics.DrawLine(p1.x, p1.y, p2.x, p2.y);
+	}
 }
 
 int main()
 {
+	for (size_t i = 0; i < NUM_POINTS; i++)
+	{
+		points.push_back(nc::Vector2{ nc::random(0.0f, 800.0f), nc::random(0.0f, 600.0f) });
+	}
 
 	char name[] = "CSC196";
 	Core::Init(name, 800, 600);
@@ -28,5 +48,5 @@ int main()
 
 	Core::GameLoop();
 	Core::Shutdown();
-}
+};
 
